9888c17b7bf7d873c98270244f602bb136a3ae3d
Michael Mwanza <mwanza.n.m@gmail.com>
2025-09-10 21:34:44 +0200

    axois flow adjusted

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import Cookies from 'js-cookie';
+import axios from "axios";
+import Cookies from "js-cookie";
 
 const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
@@ -8,18 +8,18 @@ const api = axios.create({
   withCredentials: true,
 });
 
+//fetch CSRF cookie before each request
 api.interceptors.request.use(async (config) => {
-  let token = Cookies.get('XSRF-TOKEN');
+  // Fetch CSRF cookie from Laravel
+  await axios.get(`${baseURL}/sanctum/csrf-cookie`, {
+    withCredentials: true,
+  });
 
-  if (!token) {
-    await axios.get(`${baseURL}/sanctum/csrf-cookie`, {
-      withCredentials: true,
-    });
-    token = Cookies.get('XSRF-TOKEN');
-  }
+  // Read token from cookie
+  const token = Cookies.get("XSRF-TOKEN");
 
   if (token) {
-    config.headers['X-XSRF-TOKEN'] = decodeURIComponent(token);
+    config.headers["X-XSRF-TOKEN"] = decodeURIComponent(token);
   }
 
   return config;
